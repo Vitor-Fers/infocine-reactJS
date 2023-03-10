6919322b9f3206a8fe4f8965768b19a4c571ed8b
Vitor Ferreira da Silva <vitorfers02@outlook.com>
2023-03-10 11:14:18 -0300

    search error resolved

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
-import { Link} from 'react-router-dom'
 import MovieCard from '../components/MovieCard'
 
 const searchURL = import.meta.env.VITE_SEARCH
@@ -17,6 +16,7 @@ export default function Search() {
   const getSearchMovies = async (url) => {
     const response = await fetch(url)
     const data = await response.json()
+    console.log(data)
 
     setMovies(data.results)
   }
@@ -26,17 +26,22 @@ export default function Search() {
     console.log('use effect foi chamado')
     
     const searchWithQueryURL = `${searchURL}${apiKey}&query=${query}&language=pt-BR`
+    // https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
     
     getSearchMovies(searchWithQueryURL)
   }, [query])
 
+  console.log(query)
+
   return (
     <div className='container'>
       <h2 className='titlePag'>Resultados para: <span className="query-text">{query}</span></h2>
+      {/*  */}
       <div className="movies-container">
         {movies.length === 0 && <p>Carregando...</p>}
-        {movies.length > 0 && 
-          movies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+        {console.log('carregando')}
+        {movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+        
       </div>
     </div>
   )
