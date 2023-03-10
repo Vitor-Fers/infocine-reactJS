00a916054f955d89814c034f6c44e8c65212bc34
Vitor Ferreira da Silva <vitorfers02@outlook.com>
2023-03-10 11:35:38 -0300

    retouching

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -31,17 +31,13 @@ export default function Search() {
     getSearchMovies(searchWithQueryURL)
   }, [query])
 
-  console.log(query)
 
   return (
     <div className='container'>
       <h2 className='titlePag'>Resultados para: <span className="query-text">{query}</span></h2>
-      {/*  */}
       <div className="movies-container">
         {movies.length === 0 && <p>Carregando...</p>}
-        {console.log('carregando')}
         {movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
-        
       </div>
     </div>
   )
